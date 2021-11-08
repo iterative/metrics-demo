7506295558c3c617edb596c0469cae30c488cd40
Jenifer De Figueiredo <jeny.defigueiredo@gmail.com>
2021-11-08 05:52:17 +0000

    Fix featurize.py

--- featurize.py
+++ featurize.py
@@ -22,4 +22,4 @@ with open(sys.argv[2], "w") as feat:
     data_size = os.stat(sys.argv[1]).st_size
     json.dump({"params": params_values, "work": [math.log(data_size + 1), VERSION]}, feat)
 
-# Comment to update:
+# Comment to update:7
